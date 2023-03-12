d04880ea8ad3d81198798bf99564f19ce0337059
Stephen Sinn <stevexian56@gmail.com>
2023-03-12 12:46:48 +0800

    drm/msm/sde: Trigger panel reset after first kickoff on non-60 Hz
    If we're using a refresh rate different from what the bootloader
    initializes the display with, a panel reset is necessary for our custom
    refresh rate to take effect.
    
    After the first kickoff, request a panel reset from userspace using the
    existing ESD recovery mechanism to update the refresh rate. Note that
    this is only done if the target refresh rate is *not* 60 Hz because this
    is redundant if we don't need a refresh rate update.
    
    Signed-off-by: Stephen Sinn <stevexian56@gmail.com>
    Signed-off-by: Xirudly <734428735@qq.com>

--- drivers/gpu/drm/msm/sde/sde_encoder.c
+++ drivers/gpu/drm/msm/sde/sde_encoder.c
@@ -4807,6 +4807,7 @@ void sde_encoder_kickoff(struct drm_encoder *drm_enc, bool is_error)
 	struct dsi_display_mode adj_mode;
 	ktime_t wakeup_time;
 	unsigned int i;
+	static bool first_run = true;
 
 	if (!drm_enc) {
 		SDE_ERROR("invalid encoder\n");
@@ -4852,6 +4853,25 @@ void sde_encoder_kickoff(struct drm_encoder *drm_enc, bool is_error)
 		SDE_EVT32_VERBOSE(ktime_to_ms(wakeup_time));
 		mod_timer(&sde_enc->vsync_event_timer,
 				nsecs_to_jiffies(ktime_to_ns(wakeup_time)));
+	}
+
+	/*
+	 * Trigger a panel reset if this is the first kickoff and the refresh
+	 * rate is not 60 Hz
+	 */
+	if (cmpxchg(&first_run, true, false) &&
+	    sde_enc->crtc->mode.vrefresh != 60) {
+		struct sde_connector *conn = container_of(phys->connector, struct sde_connector, base);
+		struct drm_event event = {
+			.type = DRM_EVENT_PANEL_DEAD,
+			.length = sizeof(bool)
+		};
+
+		conn->panel_dead = true;
+		event.type = DRM_EVENT_PANEL_DEAD;
+		event.length = sizeof(bool);
+		msm_mode_object_event_notify(&conn->base.base,
+			conn->base.dev, &event, (u8 *) &conn->panel_dead);
 	}
 
 	if (dsi_display && dsi_display->panel
