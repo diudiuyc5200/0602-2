fe7e12e0649c2f4edb170e2ef26fd4bff464c161
diudiuyc5200 <17766728994@163.com>
2026-06-12 19:21:33 +0800

    Update cpufreq_schedutil.c

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -235,7 +235,7 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	 * Use the frequency step below if the calculated frequency is <20%
 	 * higher than it.
 	 */
-	if (mult_frac(100, freq - h_freq, l_freq - h_freq) < 20)
+	if (mult_frac(100, freq - h_freq, l_freq - h_freq) < 30)
 		return h_freq;
 
 	return l_freq;
