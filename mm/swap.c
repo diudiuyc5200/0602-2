f3248446ef02e6cce4b0cd037859983e35c3b603
diudiuyc5200 <17766728994@163.com>
2026-06-17 17:42:29 +0800

    Update swap.c

--- mm/swap.c
+++ mm/swap.c
@@ -1046,4 +1046,7 @@ void __init swap_setup(void)
 	 * Right now other parts of the system means that we
 	 * _really_ don't want to cluster much more
 	 */
+	#if defined(CONFIG_ZRAM) || defined(CONFIG_ZSWAP)
+	page_cluster = 0;
+#endif
 }
