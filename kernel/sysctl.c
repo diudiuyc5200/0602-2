506e6302b368f99ce158009e15d1d6f2024472c1
diudiuyc5200 <17766728994@163.com>
2026-06-17 20:34:53 +0800

    Update sysctl.c

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -145,7 +145,6 @@ static int ten_thousand = 10000;
 static int six_hundred_forty_kb = 640 * 1024;
 #endif
 static int __maybe_unused two_hundred_million = 200000000;
-static int two_hundred_fifty_five = 255
 static int __maybe_unused max_kswapd_threads = MAX_KSWAPD_THREADS;
 
 /* this is needed for the proc_doulongvec_minmax of vm_dirty_bytes */
