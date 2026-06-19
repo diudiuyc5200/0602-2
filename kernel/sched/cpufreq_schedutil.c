bb7c0b9b88a61306730d8dc05f3953d0c53abde6
diudiuyc5200 <17766728994@163.com>
2026-06-19 20:14:44 +0800

    Update cpufreq_schedutil.c

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -374,7 +374,7 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 	if (!sugov_should_update_freq(sg_policy, time))
 		return;
 
-	busy = !sg_policy->need_freq_update && sugov_cpu_is_busy(sg_cpu);
+	busy = use_pelt() && sugov_cpu_is_busy(sg_cpu);
 
 	if (flags & SCHED_CPUFREQ_RT_DL) {
 		sg_policy->cached_raw_freq = 0;
