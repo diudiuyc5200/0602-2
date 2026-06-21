3fe652065d6d24d744e74248bdad5d1f9fd2f176
diudiuyc5200 <17766728994@163.com>
2026-06-21 17:22:01 +0800

    Update cpufreq_schedutil.c

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -830,14 +830,12 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us =
-				cpufreq_policy_transition_delay_us(policy);
-	tunables->down_rate_limit_us =
-				cpufreq_policy_transition_delay_us(policy);
+	tunables->up_rate_limit_us = 500;
+	tunables->down_rate_limit_us = 20000;
 	tunables->iowait_boost_enable = false;
 
 	/* Disable exponential frequency scaling by default */
-	tunables->exp_util = false;
+	tunables->exp_util = true;
 	
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
