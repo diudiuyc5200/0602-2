1db3e5aa10fcab806e17a33b60c09c168c3d717d
diudiuyc5200 <17766728994@163.com>
2026-06-23 14:01:02 +0800

    Update dsi_panel.h

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -307,6 +307,9 @@ struct dsi_panel {
 	struct dsi_read_config xy_coordinate_cmds;
 	struct dsi_read_config max_luminance_cmds;
 	struct lockdowninfo_cfg lockdowninfo_read;
+#ifdef CONFIG_DRM_SDE_EXPO
+	bool dimlayer_exposure;
+#endif
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
