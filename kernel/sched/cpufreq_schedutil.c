4e90ed31c35f7481f99a01e9bbbe66f9c243b0e6
diudiuyc5200 <17766728994@163.com>
2026-09-09 09:11:44 +0800

    优化sche

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -293,16 +293,15 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 		sugov_iowait_boost(sg_cpu, &util, &max);
 		next_f = get_next_freq(sg_policy, util, max);
 		
-		if (util > 15) {
-    /* CPU7 单独规则，优先判断 */
-    if (policy->cpu == 7) {
-        unsigned int target = policy->max * 70 / 100;
+		if (policy->cpu == 7) {
+    if (util > 15) {
+        unsigned int target = policy->max * 75 / 100;
         if (next_f < target)
             next_f = target;
     }
-    /* CPU4 单独规则 */
-    else if (policy->cpu == 4) {
-        unsigned int target = policy->max * 68 / 100;
+} else if (policy->cpu == 4) {
+    if (util > 10) {
+        unsigned int target = policy->max * 70 / 100;
         if (next_f < target)
             next_f = target;
     }
